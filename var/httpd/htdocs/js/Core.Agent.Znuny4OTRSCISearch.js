430d03bfc1d03ee7e07d3b029c91d0fa6a2c4422
Thorsten Eckel <te@znuny.com>
2015-11-30 12:04:02 +0100

    Refactoring.

--- var/httpd/htdocs/js/Core.Agent.Znuny4OTRSCISearch.js
+++ var/httpd/htdocs/js/Core.Agent.Znuny4OTRSCISearch.js
@@ -24,56 +24,61 @@ Core.Agent.Znuny4OTRSCISearch = (function (TargetNS) {
     TargetNS.Init = function ( CIClasses ) {
 
         // get default class, if given
-        var ciSearchLabel = Core.Config.Get('CISearch.Label') || ' CI Search';
-        var defaultClass = Core.Config.Get('CISearch.DefaultClassName') || '';
+        var CISearchLabel = Core.Config.Get('CISearch.Label')            || ' CI Search';
+        var DefaultClass  = Core.Config.Get('CISearch.DefaultClassName') || '';
 
-        $("#ToolBar").append('<li class="Extended SearchProfile"><form id="CISearch"></form></li>');
-        $('#CISearch')
-            .attr("action","/wgz/index.pl").attr("method","post")
-            .append('<select id="CIClassSelection" name="CIClassSelection" title="Class Selection" style="margin-right: 10px;"></select></li>')
-            .append('<input type="hidden" name="Action" value="AgentITSMConfigItemSearch">')
-            .append('<input type="hidden" name="Subaction" value="Search">')
-            .append('<input type="hidden" name="ClassID" value="" id="ClassID">')
-            .append('<input type="hidden" name="SearchDialog" value="1">')
-            .append('<input type="hidden" name="EmptySearch" value="">')
-            .append('<input type="hidden" name="Profile" value="last-search">')
-            .append('<input type="hidden" name="ProfileName" value="">')
-            .append('<input type="hidden" name="AttributeOrig" value="Name">')
-            .append('<input type="hidden" name="PreviousVersionSearch" value="0">')
-            .append('<input type="hidden" name="ResultForm" value="Normal">')
-            .append('<input type="hidden" name="Name" id="CIName" value="">')
-            .append('<input type="text" size="20" name="SearchName" id="SearchName" value="" placeholder="'+ciSearchLabel+'" title="'+ciSearchLabel+'">')
+        // ugly but most performant
+        var CISearchForm = '<li class="Extended SearchProfile">';
+        CISearchForm    += '<form id="CISearch" action="'+ OTRS.Config.Get('Baselink') +'" method="post">';
+        CISearchForm    += '<select id="ClassID" name="ClassID" title="Class Selection" style="margin-right: 10px;">';
+        CISearchForm    += '<option value="">-</option>';
 
-        // populate the class dropdown and select the default class
-        $('#CIClassSelection').append($('<option>').text('-').attr('value', '-'));
-        $.each(CIClasses, function (key, value) {
-            if(value === defaultClass){
-             $('#CIClassSelection').append($('<option>').text(value).attr('value', key).attr('selected', 'selected'));
-            }else{
-             $('#CIClassSelection').append($('<option>').text(value).attr('value', key));
+        $.each(CIClasses, function (Key, Value) {
+
+            var $Selected = '';
+            if(Value === DefaultClass) {
+                var $Selected = 'selected="selected"';
             }
+
+            CISearchForm += '<option value="'+ Value +'" '+ selected +'>'+ Key +'</option>';
         });
+
+        CISearchForm    += '</select>';
+        CISearchForm    += '<input type="text" size="20" name="SearchName" id="SearchName" value="" placeholder="'+CISearchLabel+'" title="'+CISearchLabel+'">';
+        CISearchForm    += '<input type="hidden" name="Action" value="AgentITSMConfigItemSearch">';
+        CISearchForm    += '<input type="hidden" name="Subaction" value="Search">';
+        CISearchForm    += '<input type="hidden" name="SearchDialog" value="1">';
+        CISearchForm    += '<input type="hidden" name="EmptySearch" value="">';
+        CISearchForm    += '<input type="hidden" name="Profile" value="last-search">';
+        CISearchForm    += '<input type="hidden" name="ProfileName" value="">';
+        CISearchForm    += '<input type="hidden" name="AttributeOrig" value="Name">';
+        CISearchForm    += '<input type="hidden" name="PreviousVersionSearch" value="0">';
+        CISearchForm    += '<input type="hidden" name="ResultForm" value="Normal">';
+        CISearchForm    += '<input type="hidden" name="Name" id="CIName" value="">';
+        CISearchForm    += '</form>';
+        CISearchForm    += '</li>';
+
+        $("#ToolBar").append(CISearchForm);
 
         // get config values for pre and suffix
-        var prefix = Core.Config.Get('CISearch.Prefix')||'';
-        var suffix = Core.Config.Get('CISearch.Suffix')||'';
+        var Prefix = Core.Config.Get('CISearch.Prefix') || '';
+        var Suffix = Core.Config.Get('CISearch.Suffix') || '';
+
         // register change event to pass the value from fulltext search box to the hidden name field
         $("#SearchName").on("change paste keyup", function() {
-           $("#CIName").val(prefix+$(this).val()+suffix)
+           $("#CIName").val(Prefix + $(this).val() + Suffix);
         });
 
-        // set classid from select box to the hidden classid text field
-        $( "#CISearch" ).submit(function( event ) {
+        $("#CISearch").submit(function( Event ) {
 
             //Don't submit if no class is selected
-            if ($("#CIClassSelection").val()==='-'){
-                event.preventDefault;
+            if (
+                !$("#CIClassSelection").val()
+                $("#CIClassSelection").val().length <= 0
+            ){
+                Event.preventDefault;
             }
-
-          $("#ClassID").val($("#CIClassSelection").val());
         });
-
-
 
         return true;
     }
