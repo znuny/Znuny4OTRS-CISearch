ce3628d865d89996d41984add1ab6f501134593a
Jens Pfeifer <jens.pfeifer@znuny.com>
2022-01-04 14:42:37 +0000

    Ported to Znuny 6.1.

---
+++ Kernel/Language/de_ZnunyCISearch.pm
@@ -0,0 +1,36 @@
+# --
+# Copyright (C) 2012-2022 Znuny GmbH, http://znuny.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Language::de_ZnunyCISearch;
+
+use strict;
+use warnings;
+
+use utf8;
+
+sub Data {
+    my $Self = shift;
+
+    # SysConfig
+    $Self->{Translation}->{'This configuration defines a mapping of role to class name that is used for the CI search'} = 'Diese Konfiguration definiert eine Rollen-Klassen-Zuordnung, die für die CI-Suche verwendet werden soll.';
+    $Self->{Translation}->{'This configuration defines a prefix that will be used for the CI search.'} = 'Diese Konfiguration definiert ein Präfix, das für die CI-Suche verwendet werden soll.';
+    $Self->{Translation}->{'This configuration defines a suffix that will be used for the CI search.'} = 'Diese Konfiguration definiert ein Suffix, das für die CI-Suche verwendet werden soll.';
+    $Self->{Translation}->{'This configuration defines a PreApplication module to enable Znuny-CISearch in all views.'} = 'Diese Konfiguration definiert ein Pre-Application-Modul, das die CI-Suche in allen Ansichten aktiviert.';
+    $Self->{Translation}->{'List of all JS files which are loaded to provide the CI search for the agent interface.'} = 'Liste aller JS-Dateien, die geladen werden, um die CI-Suche zu ermöglichen.';
+    $Self->{Translation}->{'CI Search'} = 'CI-Suche';
+    $Self->{Translation}->{'This configuration defines a global default class that will be used for the CI search if no other class can not get determined.'} = 'Diese Konfiguration definiert eine globale Standard-Klasse, welche für die CI-Suche genutzt wird, wenn kein andere Klasse bestimmt werden konnte.';
+    $Self->{Translation}->{'This configuration defines a mapping of role to class name that is used as default class for the CI search.'} = 'Diese Konfiguration definiert ein Mapping zwischen Rollen- und Klassennamen, welche für die CI-Suche als Standard-Klasse genutzt wird.';
+    $Self->{Translation}->{'Defines the config parameters of this item, to be shown in the preferences view.'} = 'Definiert die Konfigurationsparameter dieses Elements, die in der Einstellungsansicht angezeigt werden sollen.';
+
+    $Self->{Translation}->{'CI search default class name'} = 'CI-Suche-Standard-Klasse';
+    $Self->{Translation}->{'Default class name'}           = 'Standard-Klasse';
+
+    return 1;
+}
+
+1;
--- doc/de/feature.md
+++ doc/de/feature.md
@@ -1,48 +1,10 @@
 # Funktionalität
 
-## Toolbar CI Suche
+## CI-Suche per Toolbar
 
-Im OTRS Standard ist es nicht möglich über die Toolbar Config Items zu suchen. Diese Erweiterung erlaubt es in allen Config Item Klassen zu suchen, für die man berechtig ist.
-Dafür wird ein Dropdown Feld mit Config Item Klassen und ein Suchfeld für den Namen des Config Items zur Toolbar hinzugefügt. Dieses ist in allen Ansichten des AgentenInterface sichtbar.
+Diese Erweiterung ermöglicht es, in allen Config-Item-Klassen zu suchen, für die man berechtigt ist.
+Dafür wird ein Dropdown-Feld mit Config-Item-Klassen und ein Suchfeld für den Namen des Config-Items zur Toolbar hinzugefügt. Dieses ist in allen Ansichten des Agenten-Interface sichtbar.
 
 ## Persönliche Einstellungen
 
-In den persönlichen Einstellungen kann die "CI Suche Standard-Klasse" ausgewählt werden.
-
-## SysConfig Einstellungen
-
-Die Erweiterung kann mit drei SysConfig Einstellungen konfiguriert werden:
-
-### Znuny4OTRSCISearch::SearchParams###Prefix
-
-Diese SysConfig ermöglicht es einen Prefix Parameter für die Suche zu definieren, der an jede Eingabe im Suchfeld vorangestellt wird.
-
-### Znuny4OTRSCISearch::SearchParams###Suffix
-
-Diese SysConfig ermöglicht es einen Suffix Parameter für die Suche zu definieren, der an jede Eingabe im Suchfeld angefügt wird.
-
-### Znuny4OTRSCISearch::SearchParams###FallbackDefaultClassName
-
-Diese Konfiguration definiert eine globale Standard-Klasse, welche für die CI-Suche genutzt wird, wenn kein andere Klasse bestimmt werden konnte.
-
-Standardwert: Computer
-
-### Znuny4OTRSCISearch::SearchParams###DefaultClassName
-
-Diese Konfiguration definiert ein Mapping zwischen Rollen zu Klassen-Namen, welche für die CI-Suche als Standard-Klasse genutzt wird. Diese Konfiguration ist per default deaktiviert.
-
-Hinweis: CI Klassen Namen werden ggf. auch übersetzt!
-
-Hier ein Beispiel:
-
-Rollen-Name  =>  Klassen-Name
-
-computerrole => Computer
-
-hardwarerole => Hardware
-
-locationrole => Location
-
-networkrole  => Network
-
-softwarerole => Software
+In den persönlichen Einstellungen kann die "CI-Suche-Standard-Klasse" ausgewählt werden.
